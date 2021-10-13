30405ef8c9b2c3c2e0581ad81eb8ad5718e3fb13
Yevhen Liakh <jeka.lyah1992@gmail.com>
2021-10-13 16:26:02 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//routing
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Styles:
+import {GlobalStyle} from "./GlobalStyle";
+//Components:
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound.";
+
+const App = () => (
+    <Router>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/:movieId" element={<Movie/>}/>
+            <Route path="/*" element={<NotFound/>}/>
+        </Routes>
+        <GlobalStyle/>
+    </Router>
   );
-}
 
 export default App;
---
+++ src/components/HeroImage/HeroImage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+import {Wrapper, Content, Text } from "./HeroImage.styles";
+
+const HeroImage = (prop) => (
+    <Wrapper image={prop.image} >
+        <Content>
+            <Text>
+                <h1>
+                    {prop.title}
+                </h1>
+                <p>
+                    {prop.text}
+                </p>
+            </Text>
+        </Content>
+    </Wrapper>
+);
+
+HeroImage.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    text: PropTypes.string,
+}
+
+export default HeroImage;
